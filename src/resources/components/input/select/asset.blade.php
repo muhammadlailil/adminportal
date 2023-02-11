f7522a48493e0095584b05674f68521526ae67b7
laililmahfud <laililmahvut@gmail.com>
2023-02-11 20:02:33 +0700

    fixing style and niselect asset

--- src/resources/components/input/select/asset.blade.php
+++ src/resources/components/input/select/asset.blade.php
@@ -1,5 +1,6 @@
 @push('js')
 {{-- https://bluzky.github.io/nice-select2/ --}}
 <link rel="stylesheet" href="https://bluzky.github.io/nice-select2/dist/css/nice-select2.css">
-<script src="https://bluzky.github.io/nice-select2/dist/js/nice-select2.js"></script>
+{{-- <script src="https://bluzky.github.io/nice-select2/dist/js/nice-select2.js"></script> --}}
+<script src="{{asset('adminportal/js/nice-select2.js')}}"></script>
 @endpush
