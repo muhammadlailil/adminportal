c52b4fae6285af5a9608561fc048620c522159ea
laililmahfud <laililmahvut@gmail.com>
2023-02-11 15:06:17 +0700

    remove assign token on middleware

--- src/app/Middleware/ApiPortalMiddleware.php
+++ src/app/Middleware/ApiPortalMiddleware.php
@@ -30,7 +30,6 @@ class ApiPortalMiddleware
             if($role && in_array(@$user->role,  explode('|',$role))){
                 return $this->forbidden("You don't have access to this endpoint");
             }
-            $request['authApi'] = $user;
             return $next($request);
         }catch(\Exception $e){
             if($e instanceof ExpiredException){
