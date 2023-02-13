8f4ede4f146cfe2a56f1b3d77c88e76d34d5071b
laililmahfud <laililmahvut@gmail.com>
2023-02-13 21:00:14 +0700

    notification

--- src/resources/module/profile/index.blade.php
+++ src/resources/module/profile/index.blade.php
@@ -16,12 +16,14 @@
                                 @lang('adminportal.change_password')
                             </a>
                         </li>
+                        @if(portal_config('nofitication'))
                         <li class="list-group-item">
                             <a href="{{route('admin.profile',['view'=>'notification'])}}">
                                 <i class="isax-b icon-notification icon"></i>
                                 @lang('adminportal.notification')
                             </a>
                         </li>
+                        @endif
                     </ul>
                     <h5 class="form-title mb-3 mt-4" style="margin-left: 15px;">@lang('adminportal.profile')</h5>
                     <ul class="list-group">
@@ -58,7 +60,7 @@
         </div>
 
         @if(request('view'))
-            @include("portalmodule::profile.".request('view'))
+        @include("portalmodule::profile.".request('view'))
         @endif
     </div>
 </x-portal::layout.admin>
