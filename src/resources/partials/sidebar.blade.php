86ea3edbd2dd0db2e27868c74153fe604c620d10
Naufal Hanif <nahanifg@gmail.com>
2023-03-15 10:30:24 +0700

    adding margin bottom on logout button

--- src/resources/partials/sidebar.blade.php
+++ src/resources/partials/sidebar.blade.php
@@ -79,7 +79,7 @@
             @endif
         </ul>
     </nav>
-    <div class="nav-navigation-bottom text-white text-center ps-0">
+    <div class="nav-navigation-bottom text-white text-center ps-0 mb-5">
         <a href="javascript:;" data-toggle="confirmation" data-message="{{__('adminportal.logout_confirmation')}}" data-action="{{route('admin.auth.logout')}}"
             class="nav-link justify-content-center d-flex align-items-center p-3">
             <i class="isax icon-logout nav-icon me-2"></i>
