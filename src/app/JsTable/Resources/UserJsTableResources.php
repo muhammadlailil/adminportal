986ff7f269a159feb0f344218a7395063ae9336e
laililmahfud <laililmahvut@gmail.com>
2023-05-09 15:56:51 +0700

    update jstable condition js

--- src/app/JsTable/Resources/UserJsTableResources.php
+++ src/app/JsTable/Resources/UserJsTableResources.php
@@ -8,7 +8,7 @@ class UserJsTableResources extends JsTableResources{
 
      public function toArray($row){
           return [
-               TableHtml::bulkCheckbox($row->id),
+               TableHtml::checkbox($row->id),
                TableHtml::profileUser($row->name,$row->profile),
                $row->email,
                $row->role_name,
