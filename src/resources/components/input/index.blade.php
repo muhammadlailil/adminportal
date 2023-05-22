b9c0a166fd33d2cb3f4228126157515ab2474639
laililmahfud <laililmahvut@gmail.com>
2023-05-22 14:44:30 +0700

    help text input

--- src/resources/components/input/index.blade.php
+++ src/resources/components/input/index.blade.php
@@ -1,31 +1,37 @@
 @php
-    if(!@$attributes['required']){
+    if (!@$attributes['required']) {
         $attributes['required'] = true;
-    }else{
+    } else {
         unset($attributes['required']);
     }
     $required = @$attributes['required'];
 @endphp
-<div class="form-group {{(@$horizontal)?'row':''}} {{($errors->has($name))?'has-error':''}}">
-    <label for="" class="label {{(@$horizontal)?'col-sm-2':'text-bold'}}">
-        {{$label}}
-        @if(@$required)
-        <span class="required">*</span>
+<div class="form-group {{ @$horizontal ? 'row' : '' }} {{ $errors->has($name) ? 'has-error' : '' }}">
+    <label for="" class="label {{ @$horizontal ? 'col-sm-2' : 'text-bold' }}">
+        {{ $label }}
+        @if (@$required)
+            <span class="required">*</span>
         @endif
     </label>
-    @if(@$horizontal)
-    <div class="col-sm-6">
-        <input type="{{$type}}" {{$attributes}} name="{{$name}}" id="{{$name}}" class="form-control {{@$class}}" placeholder="{{@$placeholder}}" value="{{$slot}}">
-        @if($errors->has($name))
-        <span class="error-text">{{$errors->first($name)}}</span>
-        @endif
-        {{@$help_text}}
-    </div>
+    @if (@$horizontal)
+        <div class="col-sm-6">
+            <input type="{{ $type }}" {{ $attributes }} name="{{ $name }}" id="{{ $name }}"
+                class="form-control {{ @$class }}" placeholder="{{ @$placeholder }}" value="{{ $slot }}">
+            @if ($errors->has($name))
+                <span class="error-text">{{ $errors->first($name) }}</span>
+            @endif
+            @if (@$helpText)
+                <span class="help-text">{{ @$helpText }}</span>
+            @endif
+        </div>
     @else
-    <input type="{{$type}}" {{$attributes}} name="{{$name}}" id="{{$name}}" class="form-control {{@$class}}" placeholder="{{@$placeholder}}" value="{{$slot}}">
-    @if($errors->has($name))
-    <span class="error-text">{{$errors->first($name)}}</span>
-    @endif
-    {{@$help_text}}
+        <input type="{{ $type }}" {{ $attributes }} name="{{ $name }}" id="{{ $name }}"
+            class="form-control {{ @$class }}" placeholder="{{ @$placeholder }}" value="{{ $slot }}">
+        @if ($errors->has($name))
+            <span class="error-text">{{ $errors->first($name) }}</span>
+        @endif
+        @if (@$helpText)
+            <span class="help-text">{{ @$helpText }}</span>
+        @endif
     @endif
 </div>
